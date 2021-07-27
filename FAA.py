36674772d5cced84f8b554458ac4f67c68029b5f
seojeongmoon <55705552+seojeongmoon@users.noreply.github.com>
2021-07-27 16:26:15 +0200

    add print_mode that can be switched via argument
    add print_mode that either prints (on) or generates csv files (off)

--- FAA.py
+++ FAA.py
@@ -4,20 +4,31 @@ from glob import glob
 import os
 import sys
 
+print_mode = True
+
 dir='result'
 if len(sys.argv)>1:
-	dir=sys.argv[1]
+	if sys.argv[1] is True or sys.argv[1] is False:
+		print_mode=sys.argv[1]
+	else:
+		dir=sys.argv[1]
+	if len(sys.argv)>2:
+		print_mode=sys.argv[2]
 
 pth='data/'
 file_list = glob(pth+"*.csv")
 etf_list = []
 for filename in file_list:
     etf_list.append(filename[len(pth):len(pth)+4])
+
+if(print_mode):
+	print('etf list = ', etf_list)
+
 dm = pd.DataFrame()
 rf = pd.DataFrame()
 pl=[]
 sl=[]
-# for i in range(len(file_list)):
+
 for i, etf in enumerate(etf_list):
 	df = pd.read_csv(pth + etf + ' Historical Data.csv')
 	df.rename(columns={"Price":etf},inplace=True)
@@ -32,25 +43,59 @@ for i, etf in enumerate(etf_list):
 		dt=df[['Date',etf]]
 		dm=dm.join(dt.set_index('Date'), how='inner', on='Date')
 
-os.mkdir(dir,0o666)
-dm.to_csv(dir + "/Price Data.csv")
+# create a directory
+if(not print_mode):
+	os.mkdir(dir,0o666)
+# add a price data.csv
+if(not print_mode):
+	dm.to_csv(dir + "/Price Data.csv")
+
+# generate and add correlation data.csv
 corr=dm.corr()
 cf=corr.sum(axis=1)
-cf.to_csv(dir + "/Correlation Table.csv")
+if(print_mode):
+	print("correlation values: \n", cf)
 cf=cf.rank(axis=0).to_frame()
-sf = pd.DataFrame(index=etf_list,data=sl, columns=None)
-sf=sf.rank(axis=0)
-sf.to_csv(dir + "/Stdev Table.csv")
-pf= pd.DataFrame(index=etf_list,data=pl, columns=None)
-pf_per=pf*100.0
-pf=pf.rank(axis=0,ascending=False)
-pf.to_csv(dir + "/Profit Table.csv")
-pf=pf*2.1
+if(not print_mode):
+	cf.to_csv(dir + "/Correlation Table.csv")
 
+if(print_mode):
+	print("Correlation Table \n", cf, "\n")
+
+# generate and add a stdev data.csv
+sf = pd.DataFrame(index=etf_list,data=sl, columns=None)
+if(print_mode):
+	print("stdev values: \n", sf)
+sf=sf.rank(axis=0)
+if(not print_mode):
+	sf.to_csv(dir + "/Stdev Table.csv")
+
+if(print_mode):
+	print("Stdev Table \n", sf, "\n")
+
+# generate and add a profit data.csv
+pf= pd.DataFrame(index=etf_list,data=pl, columns=None)
+pf_per=round(pf*100.0,2)
+if(print_mode):
+	print("profit(%) values: \n", pf_per)
+pf=pf.rank(axis=0,ascending=False)
+if(not print_mode):
+	pf.to_csv(dir + "/Profit Table.csv")
+
+if(print_mode):
+	print("Profit Table \n", pf, "\n")
+
+# scale pf and add the ranks
+pf=pf*2.05
 rf['Rank']=cf.add(sf).add(pf).rank(axis=0)
 rf['Profit(%)']=pf_per
 
+# sort values by rank
 rf=rf.sort_values(by='Rank')
-print('Invest equally in Top three assets, if its profit is positive. If not, hold cash /n')
-rf.to_csv("result/Rank Table.csv")
-print(rf)
+# add rank table.csv
+if(not print_mode):
+	rf.to_csv(dir + "/Rank Table.csv")
+
+if(print_mode):
+	print('Invest equally in Top three assets, if its profit is positive. If not, hold cash /n')
+	print(rf)
